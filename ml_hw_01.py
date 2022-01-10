9d363f12e8e90fbbe6228905ff109122c685794c
cassieschatz <cassieschatz@gwu.edu>
2022-01-10 15:34:19 -0500

    ml 1

---
+++ ml_hw_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 13:12:08 2021
+
+@author: cassieschatz
+"""
+from sklearn.neighbors import KNeighborsClassifier
+
+i = 10
+
+knn = KNeighborsClassifier(i)
